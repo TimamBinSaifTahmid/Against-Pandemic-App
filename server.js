f0b4506941dcb11413a6c0fb0ed2a862b9b47e3e
TimamBinSaifTahmid <timam@iut-dhaka.edu>
2021-06-11 12:21:15 +0600

    auth backend completion

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyPerser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
 const app = express();
 const PORT = 3000;
 const knex = require("knex");
@@ -11,24 +13,86 @@ const postgres = knex({
     database: "AgainstPandemic",
   },
 });
-postgres.select("*").from("Users");
+postgres
+  .select("*")
+  .from("users")
+  .then((data) => {
+    console.log(data);
+  });
+app.use(bodyPerser.json());
 app.get("/", (req, res) => {
   res.send("This is woring");
 });
 app.post("/signin", (req, res) => {
-  res.send("sign in ");
+  postgres
+    .select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        postgres
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json("cant find user"));
+      }
+      else{
+        res.status(400).json("wrong credential"));
+      }
+    })
+    .catch((err) => res.status(400).json("wrong credential"));
 });
 app.post("/register", (req, res) => {
-  const { nid, name, email, location, financial_condition } = req.body;
-  postgres("Users").insert({
-    nid: nid,
-    name: name,
-    email: email,
-    location: location,
-    financial_condition: financial_condition,
-  });
+  const {
+    nid,
+    name,
+    email,
+    password,
+    location,
+    contact_info,
+    financial_condition,
+  } = req.body;
+  const hash = bcrypt.hashSync(password);
+  postgres
+    .transaction((trx) => {
+      trx
+        .insert({
+          hash: hash,
+          email: email,
+        })
+        .into("login")
+        .returning("email")
+        .then((loginEmail) => {
+          return trx("users").insert({
+            nid: nid,
+            name: name,
+            email: loginEmail[0],
+            location: location,
+            contact_info: contact_info,
+            financial_condition: financial_condition,
+          });
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    })
+    .catch((err) => res.status(400).json("unable to register"));
   res.send("sign in ");
 });
 app.listen(PORT, () => {
   console.log(`server is running on :${PORT}`);
 });
+// bcrypt.hash("bacon", null, null, function(err, hash) {
+//   // Store hash in your password DB.
+// });
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//   // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//   // res = false
+// });
