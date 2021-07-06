ebec66bc8b9c8e0a9d38b961b68fb88361c301c8
TimamBinSaifTahmid <timam@iut-dhaka.edu>
2021-07-07 01:02:41 +0600

    email verification debugging

--- server/controller/userController.controller.js
+++ server/controller/userController.controller.js
@@ -1,10 +1,30 @@
 require("dotenv").config();
+const random = require("random");
 const { google } = require("googleapis");
 const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt-nodejs");
 CLIENT_ID = process.env.CLIENT_ID;
 CLIENT_SECRET = process.env.CLIENT_SECRET;
 REDIRECT_URI = process.env.REDIRECT_URI;
 REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+let {
+  user,
+  userCreation,
+  getVerificationCode,
+  setVerificationCode,
+} = require("../model/userModel.model");
+const knex = require("knex");
+const postgres = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "tahmid",
+    database: "AgainstPandemic",
+  },
+});
+const verificationCode = random.int((min = 1111111), (max = 9999999));
+console.log(verificationCode);
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
@@ -49,4 +69,204 @@ async function sendMail(emailAddress, verficationCode) {
     return error;
   }
 }
-module.exports = sendMail;
+
+const postRegister = (req, res) => {
+  let {
+    nid,
+    name,
+    email,
+    password,
+    location,
+    contact_info,
+    financial_condition,
+  } = req.body;
+  let hash = bcrypt.hashSync(password);
+  postgres
+    .transaction((trx) => {
+      trx
+        .insert({
+          nid: nid,
+          email: email,
+          hash: hash,
+          verificationcode: verificationCode,
+        })
+        .into("login")
+        .returning("email")
+        .then((loginEmail) => {
+          console.log(loginEmail[0], verificationCode);
+          sendMail(loginEmail[0], verificationCode)
+            .then((result) => {
+              console.log("EMAIL SENT ...", result);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          console.log(
+            nid,
+            name,
+            email,
+            password,
+            location,
+            contact_info,
+            financial_condition
+          );
+          userCreation(
+            nid,
+            name,
+            email,
+            password,
+            location,
+            contact_info,
+            financial_condition
+          );
+          setVerificationCode(verificationCode);
+          console.log(user);
+          console.log("asd" + getVerificationCode());
+          return trx("users").insert({
+            nid: nid,
+            name: name,
+            email: loginEmail[0],
+            location: location,
+            contact_info: contact_info,
+            financial_condition: financial_condition,
+          });
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    })
+    .catch((err) => res.status(400).json("unable to register"));
+  res.send("sign in ");
+};
+
+const postLogin = (req, res) => {
+  postgres
+    .select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      console.log(req.body.password);
+      if (isValid) {
+        postgres
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then((user1) => {
+            console.log(user1);
+            if (user1.verified) {
+              userCreation(
+                user1[0].nid,
+                user1[0].name,
+                user1[0].email,
+                user1[0].password,
+                user1[0].location,
+                user1[0].contact_info,
+                user1[0].financial_condition
+              );
+              console.log(user.email);
+              res.status(200).json(user[0]);
+            } else {
+              userCreation(
+                user1[0].nid,
+                user1[0].name,
+                user1[0].email,
+                user1[0].password,
+                user1[0].location,
+                user1[0].contact_info,
+                user1[0].financial_condition
+              );
+              res.status(405).json("email not varified");
+            }
+          })
+          .catch((err) => res.status(400).json("cant find user"));
+      } else {
+        res.status(400).json("wrong credential");
+      }
+    })
+    .catch((err) => res.status(400).json("wrong credential"));
+};
+const isVerified = (req, res) => {
+  console.log(user.email);
+  postgres
+    .select("*")
+    .from("login")
+    .where("email", "=", user.email)
+    .then((user1) => {
+      if (user1.verificationcode == req.verficationCode) {
+        setVerificationCode();
+        console.log("verified");
+        postgres("login")
+          .where("email", "=", user.email)
+          .update({
+            verified: 1,
+          })
+          .then(() => {
+            res.status(200).send("success");
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(400).send("failed");
+          });
+      } else res.status(405).json("email not varified");
+    })
+    .catch((err) => res.status(400).json("cant find user"));
+  console.log(getVerificationCode());
+};
+const postHelpRequest = (req, res) => {
+  const reason = req.body.reason;
+  const condition = req.body.condition;
+  const type = req.body.type;
+  let date_ob = new Date();
+  let date1 = ("0" + date_ob.getDate()).slice(-2);
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+  let year = date_ob.getFullYear();
+  let date = year + "-" + month + "-" + date1;
+  postgres
+    .select("date - " + date + "::date")
+    .from("needhelp")
+    .where("nid", "=", user.nid)
+    .then((user1) => {
+      if (user > 20) {
+        postgres
+          .insert({
+            nid: user.nid,
+            location: user.location,
+            contact_info: user.contact_info,
+            financial_Condition: financial_condition,
+            reason: reason,
+            current_situation: condition,
+            type: type,
+          })
+          .into("needhelp")
+          .then(() => {
+            res.send("success");
+          })
+          .catch((err) => {
+            res.send("fail");
+          });
+      } else {
+        res.send("already asked");
+      }
+    })
+    .catch((err) => {
+      console.log("new asd");
+      postgres
+        .insert({
+          nid: user.nid,
+          location: user.location,
+          contact_info: user.contact_info,
+          financial_Condition: user.financial_condition,
+          reason: reason,
+          current_situation: condition,
+          type: type,
+        })
+        .into("needhelp")
+        .then(() => {
+          res.send("success");
+        })
+        .catch((err) => {
+          res.send("fail");
+        });
+    });
+};
+module.exports = { postRegister, postLogin, isVerified, postHelpRequest };
--- server/server.js
+++ server/server.js
@@ -1,154 +1,32 @@
 const express = require("express");
 const bodyPerser = require("body-parser");
-const bcrypt = require("bcrypt-nodejs");
-const nodemailer = require("nodemailer");
-const sendMail = require("./controller/userController.controller");
+
+const {
+  postRegister,
+  postLogin,
+  isVerified,
+  postHelpRequest,
+} = require("./controller/userController.controller");
 let {
   user,
   userCreation,
   getVerificationCode,
   setVerificationCode,
 } = require("./model/userModel.model");
-const { google } = require("googleapis");
-const random = require("random");
+
 const app = express();
 const PORT = 3000;
-const verificationCode = random.int((min = 1111111), (max = 9999999));
-console.log(verificationCode);
-const knex = require("knex");
-const postgres = knex({
-  client: "pg",
-  connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "tahmid",
-    database: "AgainstPandemic",
-  },
-});
-postgres
-  .select("*")
-  .from("users")
-  .then((data) => {
-    console.log(data);
-  });
+
 app.use(bodyPerser.json());
 app.get("/", (req, res) => {
   res.send("This is woring");
 });
-app.post("/signin", (req, res) => {
-  postgres
-    .select("email", "hash")
-    .from("login")
-    .where("email", "=", req.body.email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      console.log(req.body.password);
-      if (isValid) {
-        postgres
-          .select("*")
-          .from("users")
-          .where("email", "=", req.body.email)
-          .then((user) => {
-            if (user.verified) {
-              userCreation(
-                user[0].nid,
-                user[0].name,
-                user[0].email,
-                user[0].password,
-                user[0].location,
-                user[0].contact_info,
-                user[0].financial_condition
-              );
-              console.log(user[0].email);
-              res.status(200).json(user[0]);
-            } else res.status(405).json("email not varified");
-          })
-          .catch((err) => res.status(400).json("cant find user"));
-      } else {
-        res.status(400).json("wrong credential");
-      }
-    })
-    .catch((err) => res.status(400).json("wrong credential"));
-});
-app.post("/register", (req, res) => {
-  let {
-    nid,
-    name,
-    email,
-    password,
-    location,
-    contact_info,
-    financial_condition,
-  } = req.body;
-  let hash = bcrypt.hashSync(password);
-  postgres
-    .transaction((trx) => {
-      trx
-        .insert({
-          nid: nid,
-          email: email,
-          hash: hash,
-          verificationcode: verificationCode,
-        })
-        .into("login")
-        .returning("email")
-        .then((loginEmail) => {
-          sendMail(loginEmail[0], verificationCode)
-            .then((result) => {
-              console.log("EMAIL SENT ...", result);
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-          console.log(
-            nid,
-            name,
-            email,
-            password,
-            location,
-            contact_info,
-            financial_condition
-          );
-          userCreation(
-            nid,
-            name,
-            email,
-            password,
-            location,
-            contact_info,
-            financial_condition
-          );
-          setVerificationCode(verificationCode);
-          console.log(user);
-          console.log("asd" + getVerificationCode());
-          return trx("users").insert({
-            nid: nid,
-            name: name,
-            email: loginEmail[0],
-            location: location,
-            contact_info: contact_info,
-            financial_condition: financial_condition,
-          });
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
-    })
-    .catch((err) => res.status(400).json("unable to register"));
-  res.send("sign in ");
-});
+app.post("/signin", postLogin);
+app.post("/register", postRegister);
 
-app.post("/emailVerification", (req, res) => {
-  const verficationCode = req.body.verificationCode;
-  console.log(getVerificationCode());
-  if (verficationCode == getVerificationCode()) {
-    res.status(200).send("success");
-  } else res.status(400).send("failed");
-});
+app.post("/emailVerification", isVerified);
 
-app.post("/helpForm", (req, res) => {
-  const reason = req.body.reason;
-  const condition = req.body.condition;
-});
+app.post("/helpForm", postHelpRequest);
 
 app.listen(PORT, () => {
   console.log(`server is running on :${PORT}`);
